a7ab299d264ddb1b69fef65ed3b4b14396b998e3
kuaiyu <532560194@qq.com>
2019-05-04 20:35:37 +0800

    delete-rest-code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const path = require('path')
+// const path = require('path')
 const express = require('express')
 const SocketIO = require('socket.io')
 
 const app = express() 
-const port = 3000
+const port = 3005
 const server = app.listen(port, () => {
   console.log('server listening on port', port)
 })
@@ -13,16 +13,27 @@ const io = SocketIO(server)
 // app.use(express.static(path.join(__dirname, './dist')))
 
 const pixelData = [
-  ['red', 'red', 'blue', 'white'],
-  ['red', 'red', 'blue', 'white'],
-  ['red', 'red', 'blue', 'white'],
-  ['red', 'red', 'blue', 'white']
+  ['red', 'black', 'blue', 'green'],
+  ['red', 'black', 'blue', 'green'],
+  ['red', 'black', 'blue', 'green'],
+  ['red', 'black', 'blue', 'green']
 ]
 
-io.on('connection', (ws) => {
-  ws.emit('pixel-data', pixelData)
+var clients = []
 
-  ws.on('disconnect', () => {
+io.on('connection', (socket) => {
+  clients.push(socket)
+
+  socket.emit('initial-pixel-data', pixelData)
+
+  socket.on('draw-dot', ({row, col, color}) => {
+    pixelData[row][col] = color
+    socket.broadcast.emit('update-dot', {row, col, color})
+    socket.emit('update-dot', {row, col, color})
+  })
+
+  socket.on('disconnect', () => {
+    clients = clients.filter(it => it != socket)
     console.log('someone leaves')
   })
 })
