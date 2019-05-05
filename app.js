d3e6f3f465182ffdbf0d5681530359c91d0d06e7
kuaiyu <532560194@qq.com>
2019-05-05 21:59:54 +0800

    zoom drag pickColor drawTime onlineCount

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const path = require('path')
+const path = require('path')
 const express = require('express')
 const SocketIO = require('socket.io')
 const Jimp = require('jimp')
@@ -12,34 +12,48 @@ const server = app.listen(port, () => {
 
 const io = SocketIO(server)
 
-// app.use(express.static(path.join(__dirname, './dist')))
+async function main() {
+  const pixelData = await Jimp.read('./pixelData.png')
+  let onlineCount = 0
+  io.on('connection', async (socket) => {
+    onlineCount++
 
-const pixelData = new Jimp(20, 20, 0xffff00ff)
+    io.emit('online-count', onlineCount)
 
-io.on('connection', async (socket) => {
-  var pngBuffer = await pixelData.getBufferAsync(Jimp.MIME_PNG)
-  socket.emit('initial-pixel-data', pngBuffer)
-
-  socket.on('draw-dot', async ({row, col, color}) => {
-    var hexColor = Jimp.cssColorToHex(color)
-
-    pixelData.setPixelColor(hexColor, col, row)
-    // pixelData[row][col] = color
-
-    socket.broadcast.emit('update-dot', {row, col, color})
-    socket.emit('update-dot', {row, col, color})
-
-    var buf = await pixelData.getBufferAsync(Jimp.MIME_PNG)
-    fs.writeFile('./pixelData.png', buf, (err) => {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log('save pixel data success!')
+    var pngBuffer = await pixelData.getBufferAsync(Jimp.MIME_PNG)
+    var lastDrawTime = Date.now()
+    socket.emit('initial-pixel-data', pngBuffer)
+  
+    socket.on('draw-dot', async ({row, col, color}) => {
+      var now = Date.now()
+      if(now - lastDrawTime < 3000) {
+        return
       }
+      lastDrawTime = now
+      var hexColor = Jimp.cssColorToHex(color)
+  
+      pixelData.setPixelColor(hexColor, col, row)
+      // pixelData[row][col] = color
+  
+      io.emit('update-dot', {row, col, color})
+      // socket.broadcast.emit('update-dot', {row, col, color})
+      // socket.emit('update-dot', {row, col, color})
+  
+      var buf = await pixelData.getBufferAsync(Jimp.MIME_PNG)
+      fs.writeFile('./pixelData.png', buf, (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('save pixel data success!')
+        }
+      })
+    })
+  
+    socket.on('disconnect', () => {
+      onlineCount--
+      console.log('someone leaves')
     })
   })
+}
 
-  socket.on('disconnect', () => {
-    console.log('someone leaves')
-  })
-})
+main()
