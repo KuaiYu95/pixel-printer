354f027e1b21854a17e2d203bc00b1b3ac233c98
kuaiyu <532560194@qq.com>
2019-05-05 11:57:43 +0800

    add api-Jimp and save png function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // const path = require('path')
 const express = require('express')
 const SocketIO = require('socket.io')
+const Jimp = require('jimp')
+const fs = require('fs')
 
 const app = express() 
 const port = 3005
@@ -12,28 +14,32 @@ const io = SocketIO(server)
 
 // app.use(express.static(path.join(__dirname, './dist')))
 
-const pixelData = [
-  ['red', 'black', 'blue', 'green'],
-  ['red', 'black', 'blue', 'green'],
-  ['red', 'black', 'blue', 'green'],
-  ['red', 'black', 'blue', 'green']
-]
+const pixelData = new Jimp(20, 20, 0xffff00ff)
 
-var clients = []
+io.on('connection', async (socket) => {
+  var pngBuffer = await pixelData.getBufferAsync(Jimp.MIME_PNG)
+  socket.emit('initial-pixel-data', pngBuffer)
 
-io.on('connection', (socket) => {
-  clients.push(socket)
+  socket.on('draw-dot', async ({row, col, color}) => {
+    var hexColor = Jimp.cssColorToHex(color)
 
-  socket.emit('initial-pixel-data', pixelData)
+    pixelData.setPixelColor(hexColor, col, row)
+    // pixelData[row][col] = color
 
-  socket.on('draw-dot', ({row, col, color}) => {
-    pixelData[row][col] = color
     socket.broadcast.emit('update-dot', {row, col, color})
     socket.emit('update-dot', {row, col, color})
+
+    var buf = await pixelData.getBufferAsync(Jimp.MIME_PNG)
+    fs.writeFile('./pixelData.png', buf, (err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('save pixel data success!')
+      }
+    })
   })
 
   socket.on('disconnect', () => {
-    clients = clients.filter(it => it != socket)
     console.log('someone leaves')
   })
 })
